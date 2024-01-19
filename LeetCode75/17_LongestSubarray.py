79b837885636f3556320636e7b106f53c11c3b73
JJgotBored <jasonwbere@gmail.com>
2024-01-19 16:41:52 -0500

    Created and solved q17

--- LeetCode75/17_LongestSubarray.py
+++ LeetCode75/17_LongestSubarray.py
@@ -1,37 +1,30 @@
 class Solution(object):
-    def longestOnes(self, nums, k):
+    def longestSubarray(self, nums):
         """
         :type nums: List[int]
-        :type k: int
         :rtype: int
         """
         l = len(nums)
-        max = k
-        i = 0
+        max = 0
         count = 0
-        
-        for j in range(k):
-            if(nums[j] == 0):
-                count += 1
-        
+        i = 0
+
         while((i+max) < l):
             if(nums[i+max] == 0):
                 count += 1
-            
-            if(count <= k):
+
+            if(count <= 1):
                 max += 1
             else:
                 if(nums[i] == 0):
                     count -= 1
-                i +=1
-        return max
+                i += 1
         
-
+        max -= 1
+        return max        
 def main():
     test = Solution
-    print(test.longestOnes(test, [1,1,1,0,0,0,1,1,1,1], 0))
-
-    print(test.longestOnes(test, [1,1,1,0,0,0,1,1,1,1,0], 2))
+    print(test.longestSubarray(test, [1,1,0,1]))
 
 if(__name__ == "__main__"):
     main()
