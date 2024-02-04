862ebc1398e366f75c48bbcf519f819312d29224
JJgotBored <jasonwbere@gmail.com>
2024-02-03 22:18:44 -0500

    worked on q38
    attempted refactor but broke code
    will fix tomorrow

--- LeetCode75/38_LowestCommonAncestor.py
+++ LeetCode75/38_LowestCommonAncestor.py
@@ -7,24 +7,85 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.rSearch(root,p.val, q.val)
+        if(root == None):
+            return None
+        pFound = None
+        qFound = None
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right,p,q)
+        
+        if(left != None):
+            if(left.val == p.val):
+                pFound = left
+            elif(left.val == q.val):
+                qFound = left
+            else:
+                print(left.val)
+                return left
+            
+        if(right != None):
+            if(right.val == p.val):
+                pFound = right
+            elif(right.val == q.val):
+                qFound = right
+            else:
+                print(right.val)
+                return right
+        
+        if(root.val == p.val):
+            pFound = root
+        elif(root.val == q.val):
+            qFound = root
+        
+        #print(pFound, qFound)
+        #print(root.val, p.val, q.val)
+        if(pFound == p.val and qFound == q.val):
+            return root
+        elif(pFound == p.val):
+            return pFound
+        elif(qFound == q.val):
+            return qFound
         
         return None
-    
+    """
     def rSearch(self, root: TreeNode, p, q) -> TreeNode:
         if(root == None):
             return None
-        print(root.val)
-        left = self.rSearch(root.left, p, q)
+        pFound = False
+        qFound = False
 
+        left = self.rSearch(root.left, p, q)
         right = self.rSearch(root.right,p,q)
 
+        if(left != None):
+            print(left.val)
+            if(left.val == p):
+                pFound = True
+            elif(left.val == q):
+                qFound = True
+            else:
+                return left
+            
+        if(right != None):
+            print(right.val)
+            if(right.val == p):
+                pFound = True
+            elif(right.val == q):
+                qFound = True
+            else:
+                return right
+            
+        print(pFound, qFound)
+        if(pFound == True and qFound == True):
+            return root
+        
         if(root.val == p):
             return root
         elif(root.val == q):
             return root
         return None
-
+    """
 #### Testing Functions #####
 def setup(vals: list)->TreeNode:
     if(vals == []):
@@ -59,9 +120,13 @@ def main():
     vals = [3,5,1,6,2,0,8,None,None,7,4]
     root = setup(vals)
     node1 = TreeNode(5)
-    node2 = TreeNode(1)
+    node2 = TreeNode(4)
     #printTree(root)
     test = Solution()
-    print(test.lowestCommonAncestor(root, node1, node2))
+    temp = test.lowestCommonAncestor(root, node1, node2)
+    if(temp != None):
+        print(temp.val)
+    else:
+        print("NONE")
 if(__name__ == "__main__"):
     main()
