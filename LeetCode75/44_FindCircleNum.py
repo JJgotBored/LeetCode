c97e904b341be5c121bc38c86ecd371d8a98720c
JJgotBored <jasonwbere@gmail.com>
2024-02-07 16:18:59 -0500

    Created q44
    error on test case 88
    realized solution can be improved such that helper functions are not needed.
    will fix later

---
+++ LeetCode75/44_FindCircleNum.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findCircleNum(self, isConnected: list[list[int]]) -> int:
+        if(isConnected == []):
+            return 0
+        
+        n = len(isConnected)
+        visited = {}
+        provinces = 0
+
+        for i in range(n):
+            visited[i] = False
+
+        for i in range(n):
+            if(visited[i] == False):
+                self.rSearch(isConnected, 0,visited, n)
+                provinces += 1
+
+        return provinces
+    
+    def rSearch(self, isConnected: list[list[int]], curr: int, visited: dict, n: int):
+        if(visited[curr] == True):
+            return
+        visited[curr] = True
+        for i in range(n):
+            if(isConnected[curr][i] == 1):
+                self.rSearch(isConnected, i, visited, n)
+        return 
+    
+def main():
+    input = [[1,1,0],[1,1,0],[0,0,1]]
+    test = Solution()
+    print(test.findCircleNum(input))
+
+if(__name__ == "__main__"):
+    main()
+        
