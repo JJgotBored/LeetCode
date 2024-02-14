6c9e5b306fcafff86e4eeab2cc3588fbf70e2fbb
JJgotBored <jasonwbere@gmail.com>
2024-02-14 17:41:45 -0500

    created q46

---
+++ LeetCode75/46_calcEquation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def calcEquation(self, equations: list[list[str]], values: list[float], queries: list[list[str]]) -> list[float]:
+        return None
+
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
