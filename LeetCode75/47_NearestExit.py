8994b76cc34949185e59bc6deeeb997cf13a0b1e
JJgotBored <jasonwbere@gmail.com>
2024-02-15 17:59:50 -0500

    created q47

---
+++ LeetCode75/47_NearestExit.py
@@ -0,0 +1,9 @@
+class Solution:
+    def nearestExit(self, maze: list[list[str]], entrance: list[int]) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
