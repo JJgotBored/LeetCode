c73cf3de38fc53baaabef36a60eaf3f8045ac5ce
JJgotBored <jasonwbere@gmail.com>
2024-02-21 15:54:08 -0500

    created q49

---
+++ LeetCode75/49_FindKthLargest.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        return 0
+    
+def main():
+    nums = [3,2,1,5,6,4]
+    k = 2
+    test = Solution()
+    print(test.findKthLargest(nums, k))
+
+if(__name__ == "__main__"):
+    main()
