6e240e5125b2b4a1291b968e064619eecd8c9f40
JJgotBored <jasonwbere@gmail.com>
2024-02-21 14:50:50 -0500

    created q48

---
+++ LeetCode75/48_OrangesRotting.py
@@ -0,0 +1,10 @@
+class Solution:
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        return -1
+    
+
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
