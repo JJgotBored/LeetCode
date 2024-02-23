1de48845015bf615eb7f0a0543745c616142de12
JJgotBored <jasonwbere@gmail.com>
2024-02-23 17:23:09 -0500

    Created q50

---
+++ LeetCode75/50_SmallestInfiniteSet.py
@@ -0,0 +1,16 @@
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        return
+
+    def popSmallest(self) -> int:
+        return 0
+
+    def addBack(self, num: int) -> None:
+        return 
+    
+def main():
+    test = SmallestInfiniteSet()
+
+if(__name__ == "__main__"):
+    main()
