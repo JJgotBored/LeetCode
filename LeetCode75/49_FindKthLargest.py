6aa0bfd35bfd8545c9ee0ea5b7bfda820f6e330b
JJgotBored <jasonwbere@gmail.com>
2024-02-23 16:59:43 -0500

    updated q49
    solution works but exceeds time limit when parsing long array of values

--- LeetCode75/49_FindKthLargest.py
+++ LeetCode75/49_FindKthLargest.py
@@ -8,48 +8,100 @@ class Solution:
         i = 0
 
         while(i < l):
-            if(lLen == 0 and rLen == 0):    # no ints in queue
-                lQueue.append(nums[i])
-                lLen += 1
-                i += 1
-            elif(lLen == 0):        # no ints in left queue
-                if(rLen >= k): 
-                    if(nums[i] > rQueue[rLen -1]):
-                        lQueue.append(rQueue.pop())
-                        lLen += 1
-                        rLen -= 1
-                    else:
-                        i += 1
-                else:
-                    if(nums[i] < rQueue[rLen -1]):
+            #print(lQueue, rQueue)
+            if(lLen + rLen < k):
+                if(lLen == 0 and rLen == 0):
+                   lQueue.append(nums[i])
+                   lLen += 1
+                   i += 1
+                elif(lLen == 0):
+                    if(nums[i] <= rQueue[rLen -1]):
                         rQueue.append(nums[i])
                         rLen += 1
                         i += 1
                     else:
                         lQueue.append(rQueue.pop())
-                        lQueue += 1
-                        rQueue -= 1
-            elif(rLen == 0):        # no ints in right queue
-                if(lLen >= k):
-                    if(nums[i] > lQueue[lLen-1]):
+                        lLen +=1
+                        rLen -= 1
+                elif(rLen == 0):
+                    if(nums[i] >= lQueue[lLen -1]):
                         lQueue.append(nums[i])
                         lLen += 1
                         i += 1
+                    else:
+                        rQueue.append(lQueue.pop())
+                        rLen += 1
+                        lLen -= 1
                 else:
-                    if(nums[i] > lQueue[lLen -1]):
+                    if((lQueue[lLen -1]) <= nums[i] and nums[i] <= rQueue[rLen -1]):
                         lQueue.append(nums[i])
                         lLen += 1
+                        i +=1
+                    elif(lQueue[lLen -1] > nums[i]):
+                        rQueue.append(lQueue.pop())
+                        rLen += 1
+                        lLen -= 1
+                    elif(rQueue[rLen -1] < nums[i]):
+                        lQueue.append(rQueue.pop())
+                        lLen += 1
+                        rLen -= 1
+            else:
+                if(lLen == 0):
+                    if(nums[i] <= rQueue[rLen -1]):
                         i += 1
-            else:                   # ints in both left and right queues
-                if(nums[i] < lQueue[0]):
-                    i += 1
+                    elif(rQueue[0] <= nums[i]):
+                        rQueue.pop()
+                        rQueue.insert(0, nums[i])
+                        i +=1
+                    else:
+                        lQueue.append(rQueue.pop())
+                        lLen += 1
+                        rLen -= 1
+                elif(rLen == 0):
+                    if(nums[i] <= lQueue[0]):
+                        i += 1
+                    elif(lQueue[lLen -1] <= nums[i]):
+                        lQueue.pop(0)
+                        lQueue.append(nums[i])
+                        i += 1
+                    else:
+                        rQueue.append(lQueue.pop())
+                        rLen += 1
+                        lLen -= 1
+                else:
+                    if(nums[i] <= lQueue[0]):
+                        i += 1
+                    elif(rQueue[0] <= nums[i]):
+                        rQueue.insert(0, nums[i])
+                        rLen += 1
+                        lQueue.pop(0)
+                        lLen -= 1
+                        i += 1
+                    elif(lQueue[lLen -1] <= nums[i] and nums[i] <= rQueue[rLen -1]):
+                        lQueue.pop(0)
+                        lQueue.append(nums[i])
+                        i += 1
+                    elif(nums[i] < lQueue[lLen -1]):
+                        rQueue.append(lQueue.pop())
+                        rLen += 1
+                        lLen -= 1
+                    elif(rQueue[rLen -1] < nums[i]):
+                        lQueue.append(rQueue.pop())
+                        lLen += 1
+                        rLen -= 1
+
+        while(rQueue != []):
+            lQueue.append(rQueue.pop())
+        #print(lQueue)
+        #print(rQueue)    
         
-        
-        return 0
+        return lQueue[0]
     
 def main():
-    nums = [3,2,1,5,6,4]
-    k = 2
+    #nums = [3,2,1,5,6,4]
+    #k = 2
+    nums = [3,2,3,1,2,4,5,5,6]
+    k = 4
     test = Solution()
     print(test.findKthLargest(nums, k))
 
