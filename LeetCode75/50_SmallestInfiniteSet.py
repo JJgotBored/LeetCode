7b7294e367d94193c8971e8b1069ba63eabf9552
JJgotBored <jasonwbere@gmail.com>
2024-02-25 18:19:50 -0500

    Solved q50
    -solution works but is slow.
        - speed could be improved by using dictionary instead of list

--- LeetCode75/50_SmallestInfiniteSet.py
+++ LeetCode75/50_SmallestInfiniteSet.py
@@ -1,12 +1,26 @@
 class SmallestInfiniteSet:
 
     def __init__(self):
+        self.removed = []
         return
 
     def popSmallest(self) -> int:
-        return 0
+        smallest = 1
+        for i in self.removed:
+            if(i == smallest):
+                smallest += 1
+        self.removed.append(smallest)
+        self.removed.sort()
+
+        return smallest
 
     def addBack(self, num: int) -> None:
+        for i in range(len(self.removed)):
+            if(self.removed[i] == num):
+                self.removed.pop(i)
+                self.removed.sort()
+                break
+
         return 
     
 def main():
