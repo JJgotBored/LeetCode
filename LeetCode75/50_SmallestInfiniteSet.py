71318a23955ba01dff77fb7d824107e4c4f7bde1
JJgotBored <jasonwbere@gmail.com>
2024-02-26 18:06:56 -0500

    updated q50
    refactored to use dictionary instead of array

--- LeetCode75/50_SmallestInfiniteSet.py
+++ LeetCode75/50_SmallestInfiniteSet.py
@@ -1,30 +1,38 @@
 class SmallestInfiniteSet:
 
     def __init__(self):
-        self.removed = []
+        self.removed = {}
         return
 
     def popSmallest(self) -> int:
         smallest = 1
-        for i in self.removed:
-            if(i == smallest):
+        for i in range(1,len(self.removed.keys())+1):
+            #print(i)
+            if(smallest in self.removed.keys()):
                 smallest += 1
-        self.removed.append(smallest)
-        self.removed.sort()
+        self.removed[smallest] = True
+        #print(self.removed)
 
         return smallest
 
     def addBack(self, num: int) -> None:
-        for i in range(len(self.removed)):
-            if(self.removed[i] == num):
-                self.removed.pop(i)
-                self.removed.sort()
-                break
+        if(num in self.removed.keys()):
+            self.removed.pop(num)
 
+        #print(self.removed)
         return 
     
 def main():
     test = SmallestInfiniteSet()
+    print(test.addBack(2))
+    print(test.popSmallest())
+    print(test.popSmallest())
+    print(test.popSmallest())
+    print(test.addBack(1))
+    print(test.popSmallest())
+    print(test.popSmallest())
+    print(test.popSmallest())
+
 
 if(__name__ == "__main__"):
     main()
