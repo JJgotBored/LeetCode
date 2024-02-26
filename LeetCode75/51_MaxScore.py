8cf231ed04f87d5d68d92efb8df169fc75212751
JJgotBored <jasonwbere@gmail.com>
2024-02-26 18:08:41 -0500

    created q51

---
+++ LeetCode75/51_MaxScore.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxScore(self, nums1: list[int], nums2: list[int], k: int) -> int:
+        return 0
+
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
