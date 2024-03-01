1f62e09fb94daebef723bf9a4cb5f917dbdd3a2b
JJgotBored <jasonwbere@gmail.com>
2024-03-01 16:59:21 -0500

    Updated q51
    attempted to use solution based off of functional c++ code but failed to pass test case 3

--- LeetCode75/51_MaxScore.py
+++ LeetCode75/51_MaxScore.py
@@ -5,14 +5,32 @@ class Solution:
         pairs = []
         subset = []
         curSum = 0
+        sum = 0
         tempMax = 0
         index = 0
+        answer = -1
 
         for i in range(l):
             pairs.append([nums2[i], nums1[i]])  
         pairs.sort(reverse = True)
-        print(pairs)
+        #print(pairs)
         
+        for i in range(k-1):
+            subset.append(pairs[i])
+            sum += pairs[i][1]
+        
+        for i in range(k-1, l):
+            #print(i)
+            curSum = pairs[i][1] + sum
+            answer = max(answer, curSum*pairs[i][0])
+
+            if( k > 1 and pairs[i][1] > subset[k-2][1]):
+                sum -= subset[k-2][1]
+                subset.pop()
+                sum += pairs[i][1]
+                subset.append(pairs[i])
+
+        """
         for i in pairs:
             subset.append(i)
             curSum += i[1]
@@ -29,20 +47,21 @@ class Solution:
                         index = j
                 curSum -= subset[index][1]
                 subset.pop(index)
-        
-        print(subset)
-        return curSum * subset[k-1][0]
+        """
+        #print(subset)
+        return answer
+
 
 def main():
-    #nums1 = [1,3,3,2]
-    #nums2 = [2,1,3,4]
-    #k = 3
+    nums1 = [1,3,3,2]
+    nums2 = [2,1,3,4]
+    k = 3
     #nums1 = [4,2,3,1,1]
     #nums2 = [7,5,10,9,6]
     #k = 1
-    nums1 = [22,5,25,15,28,1]
-    nums2 = [22,30,25,25,9,18]
-    k = 3
+    #nums1 = [22,5,25,15,28,1]
+    #nums2 = [22,30,25,25,9,18]
+    #k = 3
     test = Solution()
     print(test.maxScore(nums1, nums2, k))
 
