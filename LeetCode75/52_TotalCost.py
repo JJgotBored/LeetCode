4ed68787c0abab1a6e35a146ae3645ed4d6b4c75
JJgotBored <jasonwbere@gmail.com>
2024-03-04 15:14:00 -0500

    Updated q52

--- LeetCode75/52_TotalCost.py
+++ LeetCode75/52_TotalCost.py
@@ -1,8 +1,18 @@
 class Solution:
     def totalCost(self, costs: list[int], k: int, candidates: int) -> int:
+
+        pairs = []
+
+        for i in range(len(costs)):
+            pairs.append([costs[i], i])
+
+        #pairs.sort(reverse = True)
+
         return 0
     
 def main():
+    costs = [17,12,10,2,7,2,11,20,8]
+    k = 3
     test = Solution()
 
 if(__name__ == "__main__"):
