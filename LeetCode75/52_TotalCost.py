87f741cbaa0382437a1f53c4992fb8d1e3492e69
JJgotBored <jasonwbere@gmail.com>
2024-03-04 14:09:37 -0500

    Created q52

---
+++ LeetCode75/52_TotalCost.py
@@ -0,0 +1,10 @@
+class Solution:
+    def totalCost(self, costs: list[int], k: int, candidates: int) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
+        
