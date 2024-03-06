506670280b03be5ccbf0ae591ecce366b75c8aca
JJgotBored <jasonwbere@gmail.com>
2024-03-06 18:22:09 -0500

    Solved q53

--- LeetCode75/53_GuessNumber.py
+++ LeetCode75/53_GuessNumber.py
@@ -4,13 +4,42 @@
 #          1 if num is lower than the picked number
 #          otherwise return 0
 # def guess(num: int) -> int:
+number = 6
+def guess(n: int)-> int:
+    if(n == number):
+        return 0
+    elif(n < number):
+        return 1
+
+    return -1
 
 class Solution:
     def guessNumber(self, n: int) -> int:
-        return 0
+        minNum = 1
+        maxNum = n + 1
+
+        curGuess = int((minNum + maxNum)/2)
+        result = guess(curGuess)
+
+        while(result != 0):
+            if(result < 0):
+                maxNum = curGuess
+                curGuess = int((minNum + maxNum)/2)
+            else:
+                minNum = curGuess
+                curGuess = int((minNum + maxNum)/2)
+
+            result = guess(curGuess)
+
+        #print(curGuess)
+        #print(guess(curGuess))
+        return curGuess
+    
+
     
 def main():
     test = Solution()
+    print(test.guessNumber(10))
 
 if(__name__ == "__main__"):
     main()
