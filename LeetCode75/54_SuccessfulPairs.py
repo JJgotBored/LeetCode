72dd3d272a29d7fa2fca861f81f2b7adcf0aaebb
JJgotBored <jasonwbere@gmail.com>
2024-03-06 18:28:54 -0500

    created q54

---
+++ LeetCode75/54_SuccessfulPairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def successfulPairs(self, spells: list[int], potions: list[int], success: int) -> list[int]:
+
+        return None
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
+        
