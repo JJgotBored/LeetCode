d36bdc788b72508710eda34e8a9c96eb440770c1
JJgotBored <jasonwbere@gmail.com>
2024-03-07 16:13:10 -0500

    created q56

---
+++ LeetCode75/56_MinEatingSpeed.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
+        
