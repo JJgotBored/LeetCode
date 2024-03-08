3d2a2b7bf76d625c6c51e753edfc03a1a4e8d8a0
JJgotBored <jasonwbere@gmail.com>
2024-03-08 17:29:13 -0500

    Solved q56

--- LeetCode75/56_MinEatingSpeed.py
+++ LeetCode75/56_MinEatingSpeed.py
@@ -1,9 +1,42 @@
 class Solution:
     def minEatingSpeed(self, piles: list[int], h: int) -> int:
-        return 0
+        n = len(piles)
+        piles.sort()
+        left = 1
+        right = piles[n-1]
+        mid = 0
+
+        if(h == n):
+            return piles[n-1]
+        
+        while(left < right):
+            mid = (left + right)//2
+            if(self.checkEaten(piles, h, mid)):
+                right = mid
+            else:
+                left = mid +1
+        return left
+    
+    def checkEaten(self, piles: list[int], h: int, speed: int) -> bool:
+        count = 0
+        for i in piles:
+            count += i//speed
+            if(i%speed > 0):
+                count += 1
+
+        return count <= h
     
 def main():
+    #piles = [3,6,7,11]
+    #h = 8
+
+    piles = [30,11,23,4,20]
+    #h = 5
+    h = 6
+
     test = Solution()
+
+    print(test.minEatingSpeed(piles, h))
 
 if(__name__ == "__main__"):
     main()
