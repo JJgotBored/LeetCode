fdbe58985c25106db230f8f6a069d6a6735d001e
JJgotBored <jasonwbere@gmail.com>
2024-03-11 17:33:57 -0400

    create q58

---
+++ LeetCode75/58_CombinationSum3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> list[list[int]]:
+        return [[]]
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
+        
