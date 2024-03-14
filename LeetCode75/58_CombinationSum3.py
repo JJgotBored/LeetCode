a6f248e7611fbb5966a28e9525e9cbf7a01430aa
JJgotBored <jasonwbere@gmail.com>
2024-03-14 16:11:40 -0400

    Solved q58

--- LeetCode75/58_CombinationSum3.py
+++ LeetCode75/58_CombinationSum3.py
@@ -1,37 +1,34 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> list[list[int]]:
         out = []
-        used = {}
-        for i in range(1,10):
-            used[i] = 0
+        #used = {}
+        #for i in range(1,10):
+        #    used[i] = 0
 
-        self.recSum(k, 0, n, 0, [], used, out)
+        self.recSum(k, 0, n, 0, [], 1, out)
         return out
     
-    def recSum(self, maxDepth: int, curDepth: int, maxSum: int, curSum: int, combo: list[int], used: dict, out: list[list[int]]):
+    def recSum(self, maxDepth: int, curDepth: int, maxSum: int, curSum: int, combo: list[int], start: int, out: list[list[int]]):
         if(curDepth == maxDepth ):
             if(curSum == maxSum):
                 out.append(combo[:])
-                print(combo)
+                #print(combo)
             return
         elif(curSum >= maxSum):
             return
 
-        for i in range(curDepth + 1,10):
-            if(used[i] == 0):
-                combo.append(i)
-                curSum += i
-                used[i] = 1
-                self.recSum(maxDepth, curDepth +1, maxSum, curSum, combo, used, out)
-                combo.pop()
-                curSum -= i
-                used[i] = 0
+        for i in range(start,10):
+            combo.append(i)
+            curSum += i
+            self.recSum(maxDepth, curDepth +1, maxSum, curSum, combo, i +1, out)
+            combo.pop()
+            curSum -= i
 
         return
     
 def main():
-    k = 3
-    n = 9
+    k = 4
+    n = 1
     test = Solution()
     print(test.combinationSum3(k, n))
 
