b9a76e24e1aea28e399cd195986408d213e7092e
JJgotBored <jasonwbere@gmail.com>
2024-03-14 16:30:59 -0400

    Created q60

---
+++ LeetCode75/60_MinCostClimbingStairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        return 0
+    
+def main():
+    cost = []
+    test = Solution()
+    print(test.minCostClimbingStairs(cost))
+if(__name__ == "__main__"):
+    main()
