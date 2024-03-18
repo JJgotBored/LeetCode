a9264e6c30ce56bd2cb5c6cb491186495605c9c2
JJgotBored <jasonwbere@gmail.com>
2024-03-18 15:47:35 -0400

    created q63

---
+++ LeetCode75/63_UniquePaths.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        return 0
+    
+def main():
+    m = 3
+    n = 7
+    test = Solution()
+    print(test.uniquePaths(m,n))
+
+if(__name__ == "__main__"):
+    main()
