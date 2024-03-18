8ddeb246c7e50f10910181e7c8ec9e76e8c1af1f
JJgotBored <jasonwbere@gmail.com>
2024-03-18 15:32:21 -0400

    updated Q62
    - fails test case 11 unsure why

--- LeetCode75/62_NumTiling.py
+++ LeetCode75/62_NumTiling.py
@@ -1,9 +1,23 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        return 0
+        i = 1
+        a = 1
+        b = 1
+        c = 0
+        d = 0
+        while(i < n):
+            d = c
+            c = b
+            b = a
+            a = 2 * b + d
+            #print(a, b, c, d)
+            i += 1
+        return a
     
 def main():
+    num = 6
     test = Solution()
+    print(test.numTilings(num))
 
 if(__name__ == "__main__"):
     main()
