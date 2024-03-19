023dd7d879f020b1612138191a66bcb4f504e5f0
JJgotBored <jasonwbere@gmail.com>
2024-03-19 14:59:15 -0400

    Solved Q63

--- LeetCode75/63_UniquePaths.py
+++ LeetCode75/63_UniquePaths.py
@@ -1,6 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return 0
+        arr = [[0 for i in range(n)]for j in range(m)]
+        #arr[0][0] = 1
+
+        for i in range(m):
+            for j in range(n):
+                if(i == 0 and j == 0):
+                    arr[i][j] = 1
+                elif(i == 0):
+                    arr[i][j] = arr[i][j-1]
+                elif(j == 0):
+                    arr[i][j] = arr[i-1][j]
+                else:
+                    arr[i][j] = arr[i][j-1] + arr[i-1][j]
+
+        #print(arr)
+        return arr[m-1][n-1]
     
 def main():
     m = 3
