948825e19de92ec4f0515e941d6c7cefd1caec0d
JJgotBored <jasonwbere@gmail.com>
2024-03-19 15:05:03 -0400

    Created q64

---
+++ LeetCode75/64_LongestCommonSubsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return 0
+    
+def main():
+    s1 = ""
+    s2 = ""
+    test = Solution()
+    print(test.longestCommonSubsequence(s1,s2))
+
+if(__name__ == "__main__"):
+    main()
