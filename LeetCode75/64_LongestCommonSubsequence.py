1c4ead346658342c3305b3ab31b053af37188df0
JJgotBored <jasonwbere@gmail.com>
2024-03-20 17:50:20 -0400

    updated q64
    - my brain hurts

--- LeetCode75/64_LongestCommonSubsequence.py
+++ LeetCode75/64_LongestCommonSubsequence.py
@@ -1,10 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if(text1 == text2):
+            return len(text1)
+        l1 = len(text1)
+        l2 = len(text2)
+
+        
         return 0
     
 def main():
-    s1 = ""
-    s2 = ""
+    s1 = "ab"
+    s2 = "abc"
     test = Solution()
     print(test.longestCommonSubsequence(s1,s2))
 
