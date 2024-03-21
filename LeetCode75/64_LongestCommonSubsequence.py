c032be47c61447214fd259b2dca54219a3354437
JJgotBored <jasonwbere@gmail.com>
2024-03-21 16:24:09 -0400

    Solved q64

--- LeetCode75/64_LongestCommonSubsequence.py
+++ LeetCode75/64_LongestCommonSubsequence.py
@@ -4,9 +4,26 @@ class Solution:
             return len(text1)
         l1 = len(text1)
         l2 = len(text2)
+        s1 = list(text1.strip())
+        s2 = list(text2.strip())
 
-        
-        return 0
+        previous = [0 for i in range(l2+1)]
+        current = [0 for i in range(l2+1)]
+
+        for i in range(1, l1+1):
+            for j in range(1, l2+1):
+                if(s1[i-1] == s2[j-1]):
+                    current[j] = 1 + previous[j-1]
+                else:
+                    current[j] = max(previous[j], current[j-1])
+            previous = current[:]
+
+        #print(previous)
+        #print(current)
+        #print(s1)
+        #print(s2)
+
+        return previous[l2]
     
 def main():
     s1 = "ab"
