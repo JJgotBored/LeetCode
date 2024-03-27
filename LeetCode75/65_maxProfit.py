925013bb3671b26ef0ee2c030b7b7c8fef8493b4
JJgotBored <jasonwbere@gmail.com>
2024-03-27 15:35:47 -0400

    updated q65
    - refactored solution
    - solution seems to work correctly
    - no longer exceeds memory limit on testcase 65 however time limit is now exceeded

--- LeetCode75/65_maxProfit.py
+++ LeetCode75/65_maxProfit.py
@@ -2,27 +2,28 @@ class Solution:
     def maxProfit(self, prices: list[int], fee: int) -> int:
         n = len(prices)
 
-        value = [[0 for i in range(n)] for j in range(n)]
+        value1 = [0 for i in range(n)]
+        value2 = [0 for i in range(n)]
         for i in range(n):
-            value[0][i] =  prices[i] - prices[0] - fee
+            value1[i] =  prices[i] - prices[0] - fee
 
-        value[0][0] = 0
+        value1[0]= 0
 
         for i in range(1,n):
-            value[i][i] = max(0, value[i-1][i])
+            value2[i] = max(0, value1[i])
             for j in range(i,n):
-                value[i][j] = value[i][i] + prices[j] - prices[i] - fee
-                value[i][j] = max(value[i][j], value[i][j-1], value[i-1][j]) 
+                value2[j] = value2[i] + prices[j] - prices[i] - fee
+                value2[j] = max(value2[j], value2[j-1], value1[j])
+            value1 = value2[:] 
         
-        #print(prices)
-        #print(value)
-
-        return value[n-1][n-1]
+        #print(value1)
+        #print(value2)
+        return value2[n-1]
     
 def main():
-    #prices = [1,3,2,8,4,9]
-    prices = [1,3,7,5,10,3]
-    fee = 3
+    prices = [1,3,2,8,4,9]
+    #prices = [1,3,7,5,10,3]
+    fee = 2
     test = Solution()
     print(test.maxProfit(prices, fee))
 
