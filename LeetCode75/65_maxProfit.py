07baa89d280f71765874de0f9fa3dc6988552000
JJgotBored <jasonwbere@gmail.com>
2024-03-27 15:07:02 -0400

    updated q65
    - solution seems to work but failed test case 34 due to exceeding memory limit

--- LeetCode75/65_maxProfit.py
+++ LeetCode75/65_maxProfit.py
@@ -1,29 +1,28 @@
 class Solution:
     def maxProfit(self, prices: list[int], fee: int) -> int:
         n = len(prices)
-        holding = 0
 
         value = [[0 for i in range(n)] for j in range(n)]
         for i in range(n):
             value[0][i] =  prices[i] - prices[0] - fee
-            #value[1][i] =  prices[i] - prices[1] - fee
 
         value[0][0] = 0
-        #value[1][0] = 0
-        #value[1][1] = 0
 
         for i in range(1,n):
+            value[i][i] = max(0, value[i-1][i])
             for j in range(i,n):
-                if(holding == 0):
-                    value[i][j] = prices[j] - prices[i] - fee 
+                value[i][j] = value[i][i] + prices[j] - prices[i] - fee
+                value[i][j] = max(value[i][j], value[i][j-1], value[i-1][j]) 
+        
+        #print(prices)
+        #print(value)
 
-        print(value)
-
-        return 0
+        return value[n-1][n-1]
     
 def main():
-    prices = [1,3,2,8,4,9]
-    fee = 2
+    #prices = [1,3,2,8,4,9]
+    prices = [1,3,7,5,10,3]
+    fee = 3
     test = Solution()
     print(test.maxProfit(prices, fee))
 
