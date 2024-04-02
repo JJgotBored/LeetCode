b31fb7159535c021b82ba0debc7246864d6f640f
JJgotBored <jasonwbere@gmail.com>
2024-04-02 14:16:26 -0400

    updated q65

--- LeetCode75/65_maxProfit.py
+++ LeetCode75/65_maxProfit.py
@@ -2,28 +2,21 @@ class Solution:
     def maxProfit(self, prices: list[int], fee: int) -> int:
         n = len(prices)
 
-        value1 = [0 for i in range(n)]
-        value2 = [0 for i in range(n)]
+        value1 = -100000
+        value2 = 0
+        temp = 0
+
         for i in range(n):
-            value1[i] =  prices[i] - prices[0] - fee
-
-        value1[0]= 0
-
-        for i in range(1,n):
-            value2[i] = max(0, value1[i])
-            for j in range(i,n):
-                value2[j] = value2[i] + prices[j] - prices[i] - fee
-                value2[j] = max(value2[j], value2[j-1], value1[j])
-            value1 = value2[:] 
+            temp = value1
+            value1 = max(value1, value2 - prices[i])
+            value2 = max(value2, temp+prices[i] -fee)
         
-        #print(value1)
-        #print(value2)
-        return value2[n-1]
+        return value2
     
 def main():
-    prices = [1,3,2,8,4,9]
-    #prices = [1,3,7,5,10,3]
-    fee = 2
+    #prices = [1,3,2,8,4,9]
+    prices = [1,3,7,5,10,3]
+    fee = 3
     test = Solution()
     print(test.maxProfit(prices, fee))
 
