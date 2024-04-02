87a09d3b159ea12d2168e68ed48a42ee749bb7e1
JJgotBored <jasonwbere@gmail.com>
2024-04-02 14:19:48 -0400

    Created q66

---
+++ LeetCode75/66_MinDistance.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+
+        return 0 
+    
+def main():
+    test = Solution()
+    word1 = ""
+    word2 = ""
+
+    print(test.minDistance(word1, word2))
+
+if(__name__ == "__main__"):
+    main()
