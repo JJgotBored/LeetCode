0a2239a3145217d1fa88ddc278e42c478f12ccd7
JJgotBored <jasonwbere@gmail.com>
2024-04-03 16:39:10 -0400

    updated q66

--- LeetCode75/66_MinDistance.py
+++ LeetCode75/66_MinDistance.py
@@ -1,12 +1,29 @@
 class Solution:
+    """
+    case 1:
+        n1 > n2
+        deletes are least costly as switch does not shorten and instert will require an additional delete
+    case 2:
+        n1 == n2
+        switches are least costly as an insert will require a delete and vice versa
+    case 3:
+        n1 < n2
+        inserts are least costly as switch does not increase length and deletes will require aditional insert
+    """
     def minDistance(self, word1: str, word2: str) -> int:
+        n1 = len(word1)
+        n2 = len(word2)
+        maxActions = max(n1,n2)
+        shared = 0
+        for i in range(n1):
+            print(word1[i])
 
-        return 0 
+        return maxActions - shared
     
 def main():
     test = Solution()
-    word1 = ""
-    word2 = ""
+    word1 = "horse"
+    word2 = "ros"
 
     print(test.minDistance(word1, word2))
 
