3b0833fcb00c2fc889f1e723b5bd0a7c53b0e962
JJgotBored <jasonwbere@gmail.com>
2024-04-10 16:33:27 -0400

    created Q69

---
+++ LeetCode75/69_MinFlip.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+    a = 0
+    b = 0
+    c = 0
+
+    print(test.minFlips(a,b,c))
+
+if(__name__ == "__main__"):
+    main()
