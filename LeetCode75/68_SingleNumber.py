f79aa16e0640e524eec1a46e8e4049e14919b21f
JJgotBored <jasonwbere@gmail.com>
2024-04-10 16:29:26 -0400

    solved q68

--- LeetCode75/68_SingleNumber.py
+++ LeetCode75/68_SingleNumber.py
@@ -1,9 +1,21 @@
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        return 0
+        singles = {}
+        #n = len(nums)
+        for i in nums:
+            if(i in singles.keys()):
+                singles.pop(i)
+            else:
+                singles[i] = 1
+                
+        return list(singles.keys())[0]
     
 def main():
     test = Solution()
+    #nums = [2,2,1]
+    nums = [4,1,2,1,2]
+
+    print(test.singleNumber(nums))
 
 if(__name__ == "__main__"):
     main()
