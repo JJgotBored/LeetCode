bafd562c87e004637773408a9b9cfefef06e16ff
JJgotBored <jasonwbere@gmail.com>
2024-04-10 15:33:26 -0400

    solved q67

--- LeetCode75/67_CountBits.py
+++ LeetCode75/67_CountBits.py
@@ -1,9 +1,19 @@
 class Solution:
     def countBits(self, n: int) -> list[int]:
-        return None
+        out = []
+        out.append(0)
+        for i in range(1, n+1):
+            if(i%2 == 0):
+                out.append(out[i//2])
+            else:
+                out.append(out[i//2]+1)
+        return out
     
 def main():
     test = Solution()
+    n = 5
+
+    print(test.countBits(n))
 
 if(__name__ == "__main__"):
     main()
