1930aa5b972b06d34b4826d344e5eb995289fc68
JJgotBored <jasonwbere@gmail.com>
2024-04-18 00:06:22 -0400

    Created q71

---
+++ LeetCode75/71_SuggestedProducts.py
@@ -0,0 +1,9 @@
+class Solution:
+    def suggestedProducts(self, products: list[str], searchWord: str) -> list[list[str]]:
+        return None
+    
+def main():
+    test = Solution()
+
+if(__name__ == "__main__"):
+    main()
