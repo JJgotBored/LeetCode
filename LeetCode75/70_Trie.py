eccfc7b678c51999558cdc506ffbfe3f30339caa
JJgotBored <jasonwbere@gmail.com>
2024-04-18 00:04:20 -0400

    Solved q70

--- LeetCode75/70_Trie.py
+++ LeetCode75/70_Trie.py
@@ -46,7 +46,7 @@ class Trie:
                 return False
             else:
                 temp = temp.children[j]     
-        return False
+        return True
 
 
 # Your Trie object will be instantiated and called as such:
@@ -55,16 +55,22 @@ class Trie:
 # param_2 = obj.search(word)
 # param_3 = obj.startsWith(prefix)
 def printTrie(root: Trie):
-    printTrieNode(root.root)
+    printTrieNode(root.root, "")
     return
 
-def printTrieNode(root: TrieNode):
-
+def printTrieNode(root: TrieNode, curr: str):
+    print(curr)
+    print(root.endOfWord)
+    for i in range(26):
+        if root.children[i] != None:
+            printTrieNode(root.children[i], curr + chr(i +97))
     return
 
 def main():
     test = Trie()
     test.insert("word")
+    test.insert("worst")
+    printTrie(test)
     #print(test.root.children)
 
 if(__name__ == "__main__"):
