673a387cf3a523d1e22c1028df849d8ac5fcfd12
JJgotBored <jasonwbere@gmail.com>
2024-04-20 18:21:08 -0400

    Updated q71
    - added classes for Trie and TrieNode

--- LeetCode75/71_SuggestedProducts.py
+++ LeetCode75/71_SuggestedProducts.py
@@ -1,9 +1,21 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None for i in range(26)]
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        return
+
 class Solution:
     def suggestedProducts(self, products: list[str], searchWord: str) -> list[list[str]]:
         return None
     
 def main():
     test = Solution()
+    products = []
+    search = ""
 
 if(__name__ == "__main__"):
     main()
