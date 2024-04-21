9dc5839c1212213fac46ed5812cb868e173f1b9c
JJgotBored <jasonwbere@gmail.com>
2024-04-21 18:12:57 -0400

    Updated q71
    - added insert function to Trie class

--- LeetCode75/71_SuggestedProducts.py
+++ LeetCode75/71_SuggestedProducts.py
@@ -7,6 +7,19 @@ class Trie:
     def __init__(self):
         self.root = TrieNode()
         return
+    
+    def insert(self, word: str) -> None:
+        temp = self.root
+        tempChild = None
+        for i in (word):
+            tempChild = temp.children[ord(i) -97]
+            if(tempChild == None):
+                tempChild = TrieNode()
+            temp = tempChild
+        
+        temp.endOfWord = True
+
+        return
 
 class Solution:
     def suggestedProducts(self, products: list[str], searchWord: str) -> list[list[str]]:
