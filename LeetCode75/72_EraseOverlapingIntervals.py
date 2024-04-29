f17be7eed396e38cae933e206263ca94f9d8e911
JJgotBored <jasonwbere@gmail.com>
2024-04-29 14:29:28 -0400

    created q72

---
+++ LeetCode75/72_EraseOverlapingIntervals.py
@@ -0,0 +1,10 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+    #print()
+
+if(__name__ == "__main__"):
+    main()
