dd215d3deeeeabf97d4f5430009bd48afdd42659
JJgotBored <jasonwbere@gmail.com>
2024-05-01 18:11:12 -0400

    Updated q72
    - added collision matrix

--- LeetCode75/72_EraseOverlapingIntervals.py
+++ LeetCode75/72_EraseOverlapingIntervals.py
@@ -1,5 +1,24 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        n = len(intervals)
+        matrix = [[ 0 for i in range(n)] for j in range(n)]
+        count = 0
+
+        for i in range(n):
+            for j in range(n):
+                if(i != j):
+                    if(intervals[i][0] <= intervals[j][0] and intervals[i][1] > intervals[j][0]):
+                        matrix[i][j] = 1
+                        count += 1
+                    
+                    elif(intervals[i][0] < intervals[j][1] and intervals[i][1] >= intervals[j][1]):
+                        matrix[i][j] = 1
+                        count += 1
+
+        for i in matrix:
+            print(i)
+
+        print(count)
         return 0
     
 def main():
