db71838717bd80e78351bd6c11d9d5e7fdc053d2
JJgotBored <jasonwbere@gmail.com>
2024-05-02 17:10:48 -0400

    updated q72
    - partial solution now fails on test case 19

--- LeetCode75/72_EraseOverlapingIntervals.py
+++ LeetCode75/72_EraseOverlapingIntervals.py
@@ -1,45 +1,18 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
         n = len(intervals)
-        matrix = [[ 0 for i in range(n)] for j in range(n)]
-        sums = [0 for i in range(n)]
-        clashes = 0
         removed = 0
-        index = 0
 
-        for i in range(n):
-            for j in range(n):
-                if(i != j):
-                    if(intervals[i][0] <= intervals[j][0] and intervals[i][1] > intervals[j][0]):
-                        matrix[i][j] = 1
-                        clashes += 1
-                        sums[i] += 1
-                    
-                    elif(intervals[i][0] < intervals[j][1] and intervals[i][1] >= intervals[j][1]):
-                        matrix[i][j] = 1
-                        clashes += 1
-                        sums[i] += 1
+        print(intervals)
+        intervals.sort()
+        print(intervals)
+        interval = intervals[0]
+        for i in range(1,n):
+            if(intervals[i][0] < interval[1]):
+                removed += 1
+            else:
+                interval[1] = intervals[i][1]
 
-        for i in matrix:
-            print(i)
-
-        print(clashes)
-        print(sums)
-
-        while(clashes > 0):
-            index = sums.index(max(sums))
-
-            for i in range(n):
-                if(matrix[index][i] == 1):
-                    matrix[index][i] = 0
-                    matrix[i][index] = 0
-                    clashes -= 2
-                    sums[index] -= 1
-                    sums[i] -= 1
-                    
-            removed += 1
-            print(clashes)
-            print(sums)
         return removed
     
 def main():
@@ -47,7 +20,8 @@ def main():
     #intervals = [[1,2],[2,3],[3,4],[1,3]]
     #intervals = [[1,2],[1,2],[1,2]] 
     #intervals = [[1,2],[2,3]]
-    intervals = [[0,2],[1,3],[1,3],[2,4],[3,5],[3,5],[4,6]]
+    #intervals = [[0,2],[1,3],[2,4],[3,5],[3,5],[4,6],[1,3]]
+    intervals = [[-52,31],[-73,-26],[82,97],[-65,-11],[-62,-49],[95,99],[58,95],[-31,49],[66,98],[-63,2],[30,47],[-40,-26]]
 
     print(test.eraseOverlapIntervals(intervals))
 
