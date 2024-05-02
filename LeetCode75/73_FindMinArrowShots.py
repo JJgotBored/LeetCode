2c19f6c87a67e4b7447f431073bd3d1db015673a
JJgotBored <jasonwbere@gmail.com>
2024-05-02 17:19:51 -0400

    created q73

---
+++ LeetCode75/73_FindMinArrowShots.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMinArrowShots(self, points: list[list[int]]) -> int:
+        return 0
+    
+def main():
+    test = Solution()
+    points = [[10,16],[2,8],[1,6],[7,12]]
+    print(test.findMinArrowShots(points))
+
+if(__name__ == "__main__"):
+    main()
