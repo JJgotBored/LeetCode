69ff95d865909ed6172d93f71f1153048bf0661e
JJgotBored <jasonwbere@gmail.com>
2024-05-05 17:26:57 -0400

    Solved q73

--- LeetCode75/73_FindMinArrowShots.py
+++ LeetCode75/73_FindMinArrowShots.py
@@ -1,10 +1,22 @@
 class Solution:
     def findMinArrowShots(self, points: list[list[int]]) -> int:
-        return 0
+        points.sort(key = lambda x:x[1])
+        #print(points)
+        arrows = 0
+        lastArrow = points[0][0] -1
+
+        for i in points:
+            if(i[0] > lastArrow):
+                arrows += 1
+                lastArrow = i[1]
+
+        return arrows
     
 def main():
     test = Solution()
-    points = [[10,16],[2,8],[1,6],[7,12]]
+    #points = [[10,16],[2,8],[1,6],[7,12]]
+    #points = [[1,2],[3,4],[5,6],[7,8]]
+    points = [[1,2],[2,3],[3,4],[4,5]]
     print(test.findMinArrowShots(points))
 
 if(__name__ == "__main__"):
