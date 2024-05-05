f0bd998d889c6bb91dcc48c47fba43606c644bd5
JJgotBored <jasonwbere@gmail.com>
2024-05-05 17:30:02 -0400

    created q74

---
+++ LeetCode75/74_DailyTemperatures.py
@@ -0,0 +1,11 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+        return None
+
+def main():
+    test = Solution()
+    temperatures = []
+    print(test.dailyTemperatures(temperatures))
+
+if(__name__ == "__main__"):
+    main()
