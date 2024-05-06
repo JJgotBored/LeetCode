f140320f64495b28a431f8e4fa9128314b65a86f
JJgotBored <jasonwbere@gmail.com>
2024-05-06 17:35:39 -0400

    created q75

---
+++ LeetCode75/75_StockSpanner.py
@@ -0,0 +1,19 @@
+class StockSpanner:
+
+    def __init__(self):
+        return
+
+    def next(self, price: int) -> int:
+        return 0
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)
+
+def main():
+    print("Hello World")
+    test = StockSpanner()
+
+if(__name__ == "__main__"):
+    main()
