c7a06c917132e817879b821ff39fdb631345184a
JJgotBored <jasonwbere@gmail.com>
2024-05-07 16:56:16 -0400

    updated q75
    - refactored to leverage monotonic stack

--- LeetCode75/75_StockSpanner.py
+++ LeetCode75/75_StockSpanner.py
@@ -2,27 +2,17 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = []
-        self.length = 0
         return
 
     def next(self, price: int) -> int:
-        temp = 0
+        span = 1
+        while(len(self.stack) > 0 and self.stack[0][0] <= price):
+            span += self.stack[0][1]
+            self.stack.pop(0)
 
-        if(self.length == 0 ):
-            self.stack.append(price)
-            self.length += 1
-            return 1
-        else:
-            temp = 1
-            for i in self.stack:
-                if(price >= i):
-                    temp += 1
-                else:
-                    break
-            self.stack.insert(0, price)
-            self.length += 1
-            return temp
-        return 0
+        self.stack.insert(0,[price, span])
+
+        return span
 
 
 # Your StockSpanner object will be instantiated and called as such:
@@ -30,8 +20,15 @@ class StockSpanner:
 # param_1 = obj.next(price)
 
 def main():
-    print("Hello World")
+    #print("Hello World")
     test = StockSpanner()
+    inputs = [100, 80, 60, 70, 60, 75, 85]
+    outputs = []
+
+    for i in inputs:
+        outputs.append(test.next(i))
+
+    print(outputs)
 
 if(__name__ == "__main__"):
     main()
