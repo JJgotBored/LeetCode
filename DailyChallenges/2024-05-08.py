456c3b69aa19905781fbbc949554faae28cf65d2
JJgotBored <jasonwbere@gmail.com>
2024-05-08 16:59:00 -0400

    Solved Daily Challenge
    - Solved Daily Challenge for May 8th, 2024

--- DailyChallenges/2024-05-08.py
+++ DailyChallenges/2024-05-08.py
@@ -2,23 +2,30 @@
 
 class Solution:
     def findRelativeRanks(self, score: list[int]) -> list[str]:
+        n = len(score)
         arr = []
-        out = [0 for i in len(score)]
-        for i in range(len(score)):
+        out = [0 for i in range(n)]
+
+        for i in range(n):
             arr.append([score[i], i])
 
-        arr.sort()
+        arr.sort(reverse = True)
         
         for i in range(len(arr)):
             if(i == 0):
-                out[arr[1]] = "First"
+                out[arr[i][1]] = "Gold Medal"
+            elif(i == 1):
+                out[arr[i][1]] = "Silver Medal"
+            elif(i == 2):
+                out[arr[i][1]] = "Bronze Medal"
+            else:
+                out[arr[i][1]] = str(i+1)
 
-
-        return None
+        return out
     
 def main():
     test = Solution()
-    scores = []
+    scores = [10,3,8,9,4]
     print(test.findRelativeRanks(scores))
 
 if(__name__ == "__main__"):
